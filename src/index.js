d06147c8009083e02294fac2a7f730f79fc23fbc
David Ebenezer <davidebenezer.me@gmail.com>
2018-10-06 16:49:25 -0500

    Configure image loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('JS Skeleton');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import webpackIcon from './images/webpack_logo.png';
+
+const element =
+<div>
+  <h1>Hello World</h1>
+  <img src={webpackIcon}/>
+</div>
+// var imgIcon = document.getElementById('webpack_logo');
+// imgIcon.src = webpackIcon;
+// console.log('JS Skeleton');
+
+ReactDOM.render(
+  element,
+  document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const path    = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HWP = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   module: {
@@ -6,9 +9,30 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
-      }
-    ]
+      },
+      {
+       test: /\.scss$/,
+       use: ExtractTextPlugin.extract(
+         {
+           fallback: 'style-loader',
+           use: ['css-loader', 'sass-loader']
+         })
+     },
+     {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [{
+                loader: 'url-loader',
+                options: {
+                            limit: 8000, // Convert images < 8kb to base64 strings
+                            name: 'images/[chunkhash]-[name].[ext]'
+                }
+            }]
+          }
+        ]
   },
+  plugins:[
+        new HWP({template:path.join(__dirname, '/src/index.html')})
+    ],
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
